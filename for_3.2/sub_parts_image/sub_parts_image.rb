229df361ee93327b2d0136a584fbd68ccccb35cc
ncaq <ncaq@ncaq.net>
2015-10-23 23:23:33 +0900

    画像のクリッピングを削除し,表示領域に合わせて縮尺するようにした

--- for_3.2/sub_parts_image/sub_parts_image.rb
+++ for_3.2/sub_parts_image/sub_parts_image.rb
@@ -60,26 +60,13 @@ Plugin.create :sub_parts_image do
         return
       end
 
-      @mutex.synchronize {
-        @num = urls.length
-
-        if @height_reported
-          Delayer.new {
-            helper.reset_height
-          }
-        end
-      }
+      helper.reset_height
 
       if !helper.destroyed?
         # クリックイベント
         @ignore_event = false
 
-        if @click_sid
-          helper.signal_handler_disconnect(@click_sid)
-          @click_sid = nil
-        end
-
-        @click_sid = helper.ssc(:click) { |this, e, x, y|
+        helper.ssc(:click) { |this, e, x, y|
           # クリック位置の特定
           offset = helper.mainpart_height
 
@@ -108,16 +95,11 @@ Plugin.create :sub_parts_image do
     # コンストラクタ
     def initialize(*args)
       super
-      @num = 0
-      @height_reported = false
-      @mutex = Mutex.new
       @main_icons = []
 
-      @helper_message = helper.message[:message]
-
-      if message
+      if helper.message
         # イメージ読み込みスレッドを起こす
-        Thread.new(message) { |message|
+        Thread.new(helper.message) { |message|
           urls = message.entity
                  .select{ |entity| %i<urls media>.include? entity[:slug] }
                  .map { |entity|
@@ -146,38 +128,11 @@ Plugin.create :sub_parts_image do
                 on_image_loaded(index, pixbuf)
               }
             }.trap{ |exception|
-              puts "#{@helper_message[0..10]} #{exception}"
               error exception
             }
           end
         }.trap{ |exception| error exception }
       end
-    end
-
-    # 画像表示位置をキーにアスペクト比を求める
-    def aspect_ratio(pos)
-      case @num
-      when 1, 4
-        Rational(16, 9)
-      when 2
-        Rational(1, 1)
-      when 3
-        if pos == 0
-          Rational(6, 9)
-        else
-          Rational(20, 9)
-        end
-      else
-        Rational(16, 9)
-      end
-    end
-
-    def aspect_ratio_x(pos)
-      aspect_ratio(pos).numerator
-    end
-
-    def aspect_ratio_y(pos)
-      aspect_ratio(pos).denominator
     end
 
     # 画像を描画する座標とサイズを返す
@@ -187,95 +142,36 @@ Plugin.create :sub_parts_image do
     # ==== Return
     # Gdk::Rectangle その画像を描画する場所
     def image_draw_area(pos, canvas_width)
-      case @num
-      when 1
-        height = 1 / aspect_ratio(pos) * canvas_width
-        Gdk::Rectangle.new(0, height * pos, canvas_width, height)
-      when 2
-        width = canvas_width / 2
-        height = 1 / aspect_ratio(pos) * width
-        Gdk::Rectangle.new(width * pos, 0, width, height)
-      when 3
-        if pos == 0
-          width = Rational(6, 16) * canvas_width
-          height = 1 / aspect_ratio(pos) * width
-          Gdk::Rectangle.new(0, 0, width, height)
-        else
-          x = Rational(6, 16) * canvas_width
-          width = canvas_width - x
-          height = 1 / aspect_ratio(pos) * width
-          Gdk::Rectangle.new(x, height * (pos - 1), width, height)
-        end
-      else
-        width = canvas_width / 2
-        height = 1 / aspect_ratio(pos) * width
-        Gdk::Rectangle.new(width * (pos % 2), height * (pos / 2).floor, width, height)
-      end
-    end
-
-    # 画像を切り抜くさい、どこを切り抜くかを返す
-    # ==== Args
-    # [pos] Fixnum 画像インデックス
-    # [base_area] Gdk::Pixbuf|Gdk::Rectangle 画像の寸法
-    # [draw_area] Gdk::Rectangle 描画する場所の寸法
-    # ==== Return
-    # Gdk::Rectangle base_area内の切り抜く位置
-    def image_crop_area(pos, base_area, draw_area)
-      x_ratio = Rational(base_area.width, aspect_ratio_x(pos))
-      y_ratio = Rational(base_area.height, aspect_ratio_y(pos))
-      if x_ratio == y_ratio
-        Gdk::Rectangle.new(0, 0, base_area.width, base_area.height)
-      elsif x_ratio < y_ratio
-        height = Rational(base_area.width * aspect_ratio_y(pos), aspect_ratio_x(pos))
-        Gdk::Rectangle.new(0, (base_area.height - height) / 2, base_area.width, height)
-      else
-        width = Rational(base_area.height * aspect_ratio_x(pos), aspect_ratio_y(pos))
-        Gdk::Rectangle.new((base_area.width - width) / 2, 0, width, base_area.height)
-      end
+      width = canvas_width / @main_icons.length
+      height = width / Rational(16, 9)
+      x = width * pos
+      y = 0
+      Gdk::Rectangle.new(x, y, width, height)
     end
 
     # サブパーツを描画
     def render(context)
-      @main_icons.compact.map.with_index { |icon, pos|
+      @main_icons.compact.each.with_index { |icon, pos|
         draw_rect = image_draw_area(pos, self.width)
-        crop_rect = image_crop_area(pos, icon, draw_rect)
-        [icon, draw_rect, crop_rect]
-      }.each { |icon, draw_rect, crop_rect|
+        wscale = draw_rect.width.to_f  / icon.width
+        hscale = draw_rect.height.to_f / icon.height
+        scale = [wscale, hscale].min # アスペクト比を保ち,はみ出さない範囲のスケール
+        icon = icon.scale(icon.width * scale, icon.height * scale)
         context.save {
-          scale_x = Rational(draw_rect.width, crop_rect.width)
-          scale_y = Rational(draw_rect.height, crop_rect.height)
-
-          context.translate(draw_rect.x - (icon.width - crop_rect.width) * scale_x / 2,
-                            draw_rect.y - (icon.height - crop_rect.height) * scale_y / 2)
-          context.scale(scale_x, scale_y)
+          context.translate(draw_rect.x, draw_rect.y)
           context.set_source_pixbuf(icon)
-
-          context.clip {
-            context.rounded_rectangle(crop_rect.x, crop_rect.y, crop_rect.width, crop_rect.height, 0)
-          }
-
           context.paint
         }
       }
     end
 
     def height
-      @mutex.synchronize {
-        @height_reported = true
-        if @num == 0
-          0
-        else
-          draw_rect = image_draw_area(@num - 1, width)
-          draw_rect.y + draw_rect.height
-        end
-      }
-    end
-
-
-    private
-
-    def message
-      helper.message
+      if @main_icons.length == 0
+        0
+      else
+        draw_rect = image_draw_area(@main_icons.length - 1, self.width)
+        draw_rect.y + draw_rect.height
+      end
     end
   end
 end
